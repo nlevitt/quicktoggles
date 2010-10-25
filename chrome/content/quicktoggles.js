c8a6cfcf9d15c0a69931bdd78bc19e10bc4976f1
Noah Levitt <nlevitt@ham.(none)>
2010-10-24 21:24:17 -0700

    let instead of var most of the time

--- chrome/content/quicktoggles.js
+++ chrome/content/quicktoggles.js
@@ -1,35 +1,11 @@
-// if ("undefined" == typeof(XULSchoolChrome)) {  
-//   var XULSchoolChrome = {  
-//     /** 
-//      * Initializes this object. 
-//      */  
-//     init : function() {  
-//       this.obsService =  
-//         Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);  
-//     }  
-//   };  
-//   
-//   /** 
-//    * Constructor. 
-//    */  
-//   (function() {  
-//     this.init();  
-//   }).apply(XULSchoolChrome);  
-// };  
-
 
 if ("undefined" == typeof(QuickToggles)) {  
 	var QuickToggles = {  
 		init : function(event) {
-			// alert("init() event.target.nodeName=" + event.target.nodeName + " event.target.value=" + event.target.value);
-			var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
-			// alert("init() prefs=" + prefs);
-			// alert("init() prefs.getIntPref('network.cookie.cookieBehavior')=" + prefs.getIntPref("network.cookie.cookieBehavior"));
-			// alert('init() window.document.getElementById("quick-cookies-label")=' + window.document.getElementById("quick-cookies-label"));
+			let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
 
 			// i'm not sure why this.foo() doesn't work here, but does work elsewhere
 			QuickToggles.updateLabels("quick-cookies-label", prefs.getIntPref("network.cookie.cookieBehavior") == 0);
-			// QuickToggles.updateLabel(window.document.getElementById("quick-cookies-label"), prefs.getIntPref("network.cookie.cookieBehavior") == 0);
 			QuickToggles.updateLabels("quick-javascript-label", prefs.getBoolPref("javascript.enabled"));
 			QuickToggles.updateLabels("quick-proxy-label", prefs.getIntPref("network.proxy.type") == 1);
 		
@@ -38,7 +14,7 @@ if ("undefined" == typeof(QuickToggles)) {
 
 		updateLabel : function(label, enabled) {
 			// alert("updateLabel() label=" + label + " enabled=" + enabled);
-			var settingName = label.value.replace(/ (ON|OFF|[?]+)$/, "");
+			let settingName = label.value.replace(/ (ON|OFF|[?]+)$/, "");
 			if (enabled) {
 				label.style.color = "#080";
 				label.value = settingName + " ON";
@@ -51,10 +27,10 @@ if ("undefined" == typeof(QuickToggles)) {
 		/* updates the label with id labelId in all windows */
 		updateLabels : function(labelId, enabled) {
 			// alert("updateLabels() labelId=" + labelId + " enabled=" + enabled);
-			var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);  
-			var enumerator = wm.getEnumerator("navigator:browser");  
+			let wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);  
+			let enumerator = wm.getEnumerator("navigator:browser");  
 			while (enumerator.hasMoreElements()) {  
-				var win = enumerator.getNext();
+				let win = enumerator.getNext();
 				this.updateLabel(win.document.getElementById(labelId), enabled);
 			}
 			this.updateLabel(window.document.getElementById(labelId), enabled);
@@ -62,10 +38,10 @@ if ("undefined" == typeof(QuickToggles)) {
 		
 		/* updates the value of the element with id elementId in all windows */
 		updateValues : function(elementId, value) {
-			var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);  
-			var enumerator = wm.getEnumerator("navigator:browser");  
+			let wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);  
+			let enumerator = wm.getEnumerator("navigator:browser");  
 			while (enumerator.hasMoreElements()) {  
-				var win = enumerator.getNext();
+				let win = enumerator.getNext();
 				win.document.getElementById(elementId).value = value;
 			}
 			window.document.getElementById(elementId).value = value;
@@ -74,7 +50,7 @@ if ("undefined" == typeof(QuickToggles)) {
 		toggleCookies : function(event) {
 			// alert("toggleCookies() event.target.nodeName=" + event.target.nodeName + " event.target.value=" + event.target.value);
 		
-			var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+			let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
 		
 			if (prefs.getIntPref("network.cookie.cookieBehavior") == 0) {
 				prefs.setIntPref("network.cookie.cookieBehavior", 2);
@@ -87,13 +63,13 @@ if ("undefined" == typeof(QuickToggles)) {
 		
 		toggleJavascript : function(event) {
 			// alert("toggleJavascript() event.target.nodeName=" + event.target.nodeName + " event.target.value=" + event.target.value);
-			var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+			let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
 			prefs.setBoolPref("javascript.enabled", !prefs.getBoolPref("javascript.enabled"));
 			this.updateLabels("quick-javascript-label", prefs.getBoolPref("javascript.enabled"));
 		},
 		
 		setProxy : function(host, port) {
-			var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+			let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
 		
 			prefs.setCharPref("network.proxy.http", host);
 			prefs.setCharPref("network.proxy.ftp", host);
@@ -112,10 +88,10 @@ if ("undefined" == typeof(QuickToggles)) {
 		
 		/* if proxy is enabled and user changes proxy spec, update */
 		updateProxy : function(event) {
-			var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+			let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
 			if (prefs.getIntPref("network.proxy.type") == 1) {
-				var proxy = document.getElementById("quick-proxy-value").value.split(':');
-				var port = parseInt(proxy[1]);
+				let proxy = document.getElementById("quick-proxy-value").value.split(':');
+				let port = parseInt(proxy[1]);
 				// window.dump("toggleProxy() proxy[0]=" + proxy[0] + " port=" + port);
 				if (!isNaN(port)) {
 					this.setProxy(proxy[0], port);
@@ -128,10 +104,10 @@ if ("undefined" == typeof(QuickToggles)) {
 			// alert("toggleProxy() event.target.nodeName=" + event.target.nodeName + " event.target.value=" + event.target.value);
 			// window.dump("toggleProxy() event.target.nodeName=" + event.target.nodeName + " event.target.value=" + event.target.value);
 		
-			var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+			let prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
 			if (prefs.getIntPref("network.proxy.type") != 1) {
-				var proxy = document.getElementById("quick-proxy-value").value.split(':');
-				var port = parseInt(proxy[1]);
+				let proxy = document.getElementById("quick-proxy-value").value.split(':');
+				let port = parseInt(proxy[1]);
 				// window.dump("toggleProxy() proxy[0]=" + proxy[0] + " port=" + port);
 				if (!isNaN(port)) {
 					this.setProxy(proxy[0], port);
@@ -151,10 +127,10 @@ if ("undefined" == typeof(QuickToggles)) {
 
 try { 
 	// alert("adding window onload listeners");
-	var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);  
-	var enumerator = wm.getEnumerator("navigator:browser");  
+	let wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);  
+	let enumerator = wm.getEnumerator("navigator:browser");  
 	while (enumerator.hasMoreElements()) {  
-		var win = enumerator.getNext();
+		let win = enumerator.getNext();
 		win.addEventListener("load", QuickToggles.init, false); 
 	}
 	// alert("added window onload listeners");
